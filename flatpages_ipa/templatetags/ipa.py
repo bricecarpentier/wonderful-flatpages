592bacf85d149113a71062465117e6c2ef83764b
Brice Carpentier <brice@daknet.org>
2014-12-17 22:47:11 +0100

    added docstring + rewrote the placeholder query dict

--- flatpages_ipa/templatetags/ipa.py
+++ flatpages_ipa/templatetags/ipa.py
@@ -25,15 +25,36 @@ def ipa_js():
 
 @register.simple_tag(takes_context=True)
 def ipa_placeholder(context, name, tag, *args, **kwargs):
-    """
+    """ Outputs the content of a given placeholder, eventually wrapped in a tag
+        with x-editable suitable data-* attributes.
+        The placeholder will be editable provided the following conditions are met:
+        - the user is allowed to modify placeholders
+          (has_perm: "flatpages_placeholders.change_placeholder)
+        - a "edit" flag is set in the context
+        parameters:
+        - name: the placeholder's name
+        - tag: the wrapping html tag
+        available keywords:
+        - page_independent: The Placeholder is not linked to a page.
+                            Useful for header/footer stuff.
+                            Defaults to False.
+        - visible: the Placeholder is not visible on the page (and thus can't
+                   be in-place-edited in a conventional way).
+                   Useful for meta keywords and description.
+                   Defaults to False.
+        - type: the type of editing component to display
+        - data_*: every data_* parameter will be converted to data-* attributes
+                  on the html tag
+
     """
     # fetch placeholder content
     page = context.get('flatpage', None)
     page_independent = kwargs.get('page_independent', False)
 
-    query_dict = {'name': name}
-    if page and not page_independent:
-        query_dict['page'] = page
+    query_dict = {
+        'name': name,
+        'page': page if page and not page_independent else None,
+    }
 
     visible = kwargs.get('visible', None)
     if visible is not None:
