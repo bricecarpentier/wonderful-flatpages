33a7c457810c2401e674c328e0e771b721693ce9
Brice Carpentier <brice@daknet.org>
2015-07-29 16:28:08 +0100

    added javascript code to specifically handle image placeholders

--- flatpages_ipa/static/js/flatpages-ipa.js
+++ flatpages_ipa/static/js/flatpages-ipa.js
@@ -1,1 +1,29 @@
 /* looks for trigger classes and activate the ipa behavior */
+
+$('.editable-click').popover({html: true})
+
+$(document).on('submit', 'form.image-uploader', function(event) {
+    var $this = $(this);
+    var data = new FormData(this)
+    $.ajax({
+        method: $this.attr('method'),
+        url: $this.attr('action'),
+        data: data,
+        contentType: false,
+        cache: false,
+        processData: false,
+        async: false,
+        success: function(pk) {
+            $.ajax({
+                method: 'POST',
+                url: $this.data('next-url'),
+                data: $.param({value: pk, name: 'content'}),
+                cache: false,
+                success: function() {
+                    document.location.reload();
+                }
+            });
+        }
+    });
+    event.preventDefault();
+})
