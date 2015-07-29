6a61ff5267abcbd457716ba70a3008c34a444c04
Brice Carpentier <brice@daknet.org>
2015-07-29 19:31:44 +0100

    improved asset providing

--- flatpages_ipa/static/js/flatpages-ipa.js
+++ flatpages_ipa/static/js/flatpages-ipa.js
@@ -1,29 +1,39 @@
 /* looks for trigger classes and activate the ipa behavior */
 
-$('.editable-click').popover({html: true})
-
-$(document).on('submit', 'form.image-uploader', function(event) {
-    var $this = $(this);
-    var data = new FormData(this)
-    $.ajax({
-        method: $this.attr('method'),
-        url: $this.attr('action'),
-        data: data,
-        contentType: false,
-        cache: false,
-        processData: false,
-        async: false,
-        success: function(pk) {
-            $.ajax({
-                method: 'POST',
-                url: $this.data('next-url'),
-                data: $.param({value: pk, name: 'content'}),
-                cache: false,
-                success: function() {
-                    document.location.reload();
-                }
-            });
+$(document).ready(function() {
+    $('.editable').editable();
+    $('.wysieditable').editable({
+        wysihtml5: {
+            stylesheets: [
+                '/static/css/wysihtml5.css',
+            ]
         }
     });
-    event.preventDefault();
-})
+    $('.editable-image').popover({html: true})    
+
+    $(document).on('submit', 'form.image-uploader', function(event) {
+        var $this = $(this);
+        var data = new FormData(this)
+        $.ajax({
+            method: $this.attr('method'),
+            url: $this.attr('action'),
+            data: data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: false,
+            success: function(pk) {
+                $.ajax({
+                    method: 'POST',
+                    url: $this.data('next-url'),
+                    data: $.param({value: pk, name: 'content'}),
+                    cache: false,
+                    success: function() {
+                        document.location.reload();
+                    }
+                });
+            }
+        });
+        event.preventDefault();
+    })
+});
